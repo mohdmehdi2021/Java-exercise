6661f2d451a84adc100df871c5a30a3bd40e3fac
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2022-03-03 16:56:00 +0530

    length of string
    Question - Write a program to find the length of the string "refrigerator".

---
+++ QuestOne.java
@@ -0,0 +1,11 @@
+// Question - Write a program to find the length of the string "refrigerator".
+
+// Coder = Mohd Mehdi
+// Date = 03.03.2022 04:46PM
+
+public class QuestOne{
+ public static void main(String[] args){
+	String w = new String("refrigerator");
+	System.out.println(w.length());
+ }
+}
