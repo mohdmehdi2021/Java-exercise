325ddb3844ab9368d490ca3021ceb2bbc174f7b4
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2022-03-03 17:06:02 +0530

    'e' is present in the word 'Umbrella' or not.
    Write a program to check if the letter 'e' is present in the word 'Umbrella'.

---
+++ Qtwo.java
@@ -0,0 +1,11 @@
+// Question - Write a program to check if the letter 'e' is present in the word 'Umbrella'.
+
+// Coder = Mohd Mehdi
+// Date = 03.03.2022 04:58PM
+
+public class Qtwo{
+ public static void main(String[] args){
+	String w = new String("Umbrella");
+	System.out.println(w.contains("e"));
+ }
+}
